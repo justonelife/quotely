e512231b76cd414abe393ef9660a14a40aa472bf
justonelife <ngnhut07051998@outlook.com>
2020-03-03 15:03:34 +0700

    initial setting

---
+++ src/app.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Hello</h1>
+			</React.Fragment>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById("app")
+);
